801069fed00013bdd1dfd39fb0a3e191181a85a7
tankazuki <34229874+tankazuki@users.noreply.github.com>
2020-05-14 08:58:58 +0900

    TemplateViewを使用してIndexViewを作ってみた

--- app01/views.py
+++ app01/views.py
@@ -2,7 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
 
 
 def index(request):
     return HttpResponse('aaaaaaaaaa')
+
+
+class IndexView(TemplateView):
+    template_name = 'app01/index.html'
+
